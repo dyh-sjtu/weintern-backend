4376d5e2e449cf5df199a3e65484b00042b76fc4
dyh-sjtu <dyh_sjtu@163.com>
2017-12-27 01:08:21 +0800

    修改路由结构，配置登录和超级管理员权限

--- routes/job/jobManage.js
+++ routes/job/jobManage.js
@@ -3,6 +3,9 @@ let router = express.Router();
 let Job = require('../../models/job');
 let Comment = require('../../models/comment');
 let Category = require('../../models/category');
+let Salary = require('../../models/salary');
+let Worksite = require('../../models/worksite');
+
 // 权限中间件
 let {requiredLogin, requiredAdmin} = require('../middleware/auth');
 // 没有挂载路径的中间件，应用的每个请求都会执行该中间件
@@ -12,36 +15,39 @@ router.use(requiredLogin);
 // 获得实习岗位列表
 router.get('/weintern/job/list', requiredAdmin, (req, res) => {
 	// console.log(res.locals.job)
-	Movie.find({}).populate("category", "name")
-		.exec((err, movies) => {
+	Job.find({},(err, jobs) => {
 			if (err) {
 				console.log(err)
 			} else {
 				res.render('jobList', {
 					title: '实习岗位列表',
-					movies: movies
+					jobs: jobs
 				})
 			}
 		})
-})
+});
 
 // 实习岗位录入页
 router.get('/weintern/job/add', requiredAdmin, (req, res) => {
 	Category.find({}, (err, categories) => {
-		res.render('movieAdmin', {
-			title: '后台录入页',
-			categories: categories,
-			movie: {
-				title: '',
-				doctor: '',
-				country: '',
-				year: '',
-				poster: '',
-				flash: '',
-				summary: '',
-				language: ''
-			}
-		})
+		Salary.find({}, (err, salaries) => {
+			Worksite.find({}, (err, worksites) => {
+				res.render('jobAdd', {
+					title: '实习录入页',
+					categories: categories,
+					salaries: salaries,
+					worksites: worksites,
+					job: {
+						jobname: '',
+						desc: '',
+						jobcontent: [],
+						skill: [],
+						resumeAddr: '',
+						note: ''
+					}
+				})
+			})
+		});
 	})
 });
 
@@ -49,11 +55,11 @@ router.get('/weintern/job/add', requiredAdmin, (req, res) => {
 router.get('/weintern/job/update/:id', requiredAdmin, (req, res) => {
 	let id = req.params.id;
 	if (id) {
-		Movie.findById(id, (err, movie) => {
+		Job.findById(id, (err, job) => {
 			Category.find({}, (err, categories) => {
 				res.render('movieAdmin', {
-					title: '更新页 >' + movie.title,
-					movie: movie,
+					title: '更新页 >' + job.jobname,
+					job: job,
 					categories: categories
 				})
 			})
@@ -63,26 +69,26 @@ router.get('/weintern/job/update/:id', requiredAdmin, (req, res) => {
 
 // 保存实习岗位
 router.post('/weintern/job/new', requiredAdmin, (req, res) => {
-	let id = req.body.movie._id;
-	let movieObj = req.body.movie;
-	let _movie;
+	let id = req.body.job._id;
+	let jobObj = req.body.job;
+	let _job;
 	if (id) {
 		// console.log("更新");
-		Movie.findById(id, (err, movie) => {
+		Job.findById(id, (err, job) => {
 			if (err) {
 				console.log(err)
 			}
-			_movie = Object.assign(movie, movieObj);
-			_movie.save((err, movie) => {
+			_job = Object.assign(job, jobObj);
+			_job.save((err, job) => {
 				if (err) {
 					console.log(err)
 				}
 				// 添加
-				Category.findById(movie.category, (err, category) => {
-					if (category.movies.indexOf(movie._id) > -1) {
+				Category.findById(job.category, (err, category) => {
+					if (category.jobs.indexOf(job._id) > -1) {
 						return
 					} else {
-						category.movies.push(movie._id);
+						category.jobs.push(job._id);
 					}
 					category.save((err, category) => {
 						if (err) {
@@ -90,13 +96,13 @@ router.post('/weintern/job/new', requiredAdmin, (req, res) => {
 						}
 					});
 					// 删除
-					Category.findOne({"movies": movie._id}, (err, category) => {
-						if (category.movies.length > 0) {
-							category.movies.forEach((e, i) => {
-								if (e.toString() === movie._id.toString()) {
-									category.movies.splice(i, 1)
+					Category.findOne({"jobs": job._id}, (err, category) => {
+						if (category.jobs.length > 0) {
+							category.jobs.forEach((e, i) => {
+								if (e.toString() === job._id.toString()) {
+									category.jobs.splice(i, 1)
 								}
-							})
+							});
 							category.save((err, category) => {
 								if (err) {
 									console.log(err);
@@ -106,23 +112,22 @@ router.post('/weintern/job/new', requiredAdmin, (req, res) => {
 							})
 						}
 					})
-				})
-				
+				});
 				res.redirect('/');
 			})
 		})
 	} else {
-		_movie = new Movie(movieObj);
-		let categoryId = movieObj.category;
-		let categoryName = movieObj.categoryName;
+		_job = new Job(jobObj);
+		let categoryId = jobObj.category;
+		let categoryName = jobObj.categoryName;
 		if (categoryId || categoryName) {
-			_movie.save((err, movie) => {
+			_job.save((err, job) => {
 				if (err) {
 					console.log(err)
 				}
 				if (categoryId) {
 					Category.findById(categoryId, (err, category) => {
-						category.movies.push(_movie.id);
+						category.jobs.push(_job.id);
 						category.save((err, category) => {
 							if (err) {
 								console.log(err)
@@ -133,35 +138,35 @@ router.post('/weintern/job/new', requiredAdmin, (req, res) => {
 				} else if (categoryName) {
 					let category = new Category({
 						name: categoryName,
-						movies: [movie._id]
-					})
+						jobs: [movie._id]
+					});
 					category.save((err, category) => {
-						movie.category = category._id;
-						movie.save((err, movie) => {
-							res.redirect('/weintern/job/' + movie._id);
+						job.category = category._id;
+						job.save((err, movie) => {
+							res.redirect('/weintern/job/' + job._id);
 						})
 					})
 				}
 			})
 		} else {
 			let msg = "请选择电影分类！";
-			res.redirect(`/weintern/status?return_url=/admin/movie/add&code=0&tips=${msg}`);
+			res.redirect(`/weintern/status?return_url=/weintern/job/add&code=0&tips=${msg}`);
 		}
 	}
-})
+});
 
 // 删除实习岗位
 router.delete('/weintern/job/list', requiredAdmin, (req, res) => {
 	let id = req.query.id;
 	if (id) {
-		Category.findOne({"movies": id}, (err, category) => {
-			let index = category.movies.indexOf(id);
-			category.movies.splice(index, 1);
+		Category.findOne({"jobs": id}, (err, category) => {
+			let index = category.jobs.indexOf(id);
+			category.jobs.splice(index, 1);
 			category.save((err, category) => {
 				if (err) console.log(err);
 			})
 		});
-		Movie.remove({_id: id}, (err, movie) => {
+		Job.remove({_id: id}, (err, movie) => {
 			if (err) {
 				console.log(err)
 			} else {
@@ -176,8 +181,7 @@ router.post('/weintern/job/comment', (req, res) => {
 	// ajax提交评论直接评论已经实现，对评论人的评论暂未实现
 	let _comment = req.body.comment;
 	let comment = new Comment(_comment);
-	let content = _comment.content;
-	let movieId = _comment.movie;
+	let jobId = _comment.job;
 	console.log(_comment);
 	if (_comment.cid) {
 		Comment.findById(_comment.cid, (err, comment) => {
@@ -193,7 +197,7 @@ router.post('/weintern/job/comment', (req, res) => {
 				if (err) {
 					console.log(err)
 				} else {
-					console.log("回复评论人", comment)
+					console.log("回复评论人", comment);
 					res.redirect("/weintern/job/" + movieId)
 				}
 			})
@@ -209,7 +213,7 @@ router.post('/weintern/job/comment', (req, res) => {
 				// .exec((err, comments) => {
 				//     res.send(comments)
 				// })
-				res.redirect("/weintern/job/" + movieId)
+				res.redirect("/weintern/job/" + jobId)
 			}
 		})
 	}
