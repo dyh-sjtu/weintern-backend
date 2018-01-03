dbc6423aada3357a2ffe8e317d2f377892ee7586
dyh-sjtu <dyh_sjtu@163.com>
2018-01-04 00:09:33 +0800

    token 修改

--- config/config.js
+++ config/config.js
@@ -1,7 +1,5 @@
-module.exports = {
-	wechat: {
+exports.wechat = {
 		appid: 'wx19803e803f6b65ec',
 		secret:'4a41bb85f6a3bc21d25fbd68e370e1e4',
 		token: "dyhgj03171026"
-	}
 }
--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,9 +1,20 @@
 let express = require('express');
 let router = express.Router();
-let checkToken = require('../../util/checkToken');
+let config = require('../../config/config');
 let Category = require('../../models/category');
 
-router.get('/', checkToken.wechat);
+router.get('/', (req, res) => {
+	let echostr, nonce, signature, timestamp;
+	signature = req.query.signature;
+	timestamp = req.query.timestamp;
+	nonce = req.query.nonce;
+	echostr = req.query.echostr;
+	if (checkToken(timestamp, nonce, signature, config.wechat.token)) {
+		return res.send(echostr);
+	} else {
+		return res.end();
+	}
+});
 
 router.get('/job/categories', (req, res) => {
 	Category.find({})
@@ -15,5 +26,12 @@ router.get('/job/categories', (req, res) => {
 			});
 		})
 });
+
+function checkToken(timestamp, nonce, signature, token) {
+	let currSign, tmp;
+	tmp = [token, timestamp, nonce].sort().join("");
+	currSign = crypto.createHash("sha1").update(tmp).digest("hex");
+	return currSign === signature;
+}
 
 module.exports = router;
