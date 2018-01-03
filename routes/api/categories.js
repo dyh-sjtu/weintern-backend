130e6dc56a8c1c59bc120e1213b57369cd7440c7
dyh-sjtu <dyh_sjtu@163.com>
2018-01-03 09:49:55 +0800

    测试

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -2,16 +2,12 @@ let express = require('express');
 let router = express.Router();
 let Category = require('../../models/category');
 
-router.get('/api/categories', function *(next){
-	let categories = yield Category
-		.find({})
+router.get('/api/categories', (req, res) => {
+	Category.find({})
 		.populate("jobs", "jobname company")
-		.exec();
-	this.body = {
-		success: 1,
-		data: categories
-	};
-	console.log(this.body);
+		.exec((err, categories) => {
+			res.json(categories);
+		})
 });
 
 module.exports = router;
