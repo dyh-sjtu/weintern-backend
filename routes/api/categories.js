02d621c03f40b288f80aa5195c1ec8f278902799
dyh-sjtu <dyh_sjtu@163.com>
2018-01-04 20:33:10 +0800

    修复不能post岗位类别的bug

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -50,7 +50,7 @@ router.get('/job', (req, res) => {
 // 根据类别ID查找该类别下的
 router.post('/job/category', (req, res) => {
 	let categoryId = req.query.categoryId;
-	Job.find({category: categoryId})
+	Job.find({categoryId: categoryId})
 		.exec((err, jobs) => {
 			if (err) {
 				return res.json({
