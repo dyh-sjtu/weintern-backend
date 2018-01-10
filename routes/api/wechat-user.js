ba6f6c2d3d8d8f2a48cab238d1301eb4ebdcd8d2
dyh-sjtu <dyh_sjtu@163.com>
2018-01-10 17:33:09 +0800

    测试

--- routes/api/wechat-user.js
+++ routes/api/wechat-user.js
@@ -85,7 +85,7 @@ router.get('/wx/isFavorite', Auth.requiredOpenid, (req, res) => {
 		if (err) {
 			console.log(err)
 		}
-		if (user.likes.length > 0) {
+		if (user.likes && user.likes.length > 0) {
 			if (user.likes.indexOf(favoriteId) > -1) {
 				return res.json({
 					success: 1,
